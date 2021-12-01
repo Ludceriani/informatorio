425f263b051c3ee089fd233f5b04e81309b0ef55
Ludceriani <ludmilaceriani1@gmail.com>
2021-12-01 19:13:43 -0300

    creamos pagina de inicio

--- proyecto_infor_market/proyecto_infor_market/settings.py
+++ proyecto_infor_market/proyecto_infor_market/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'proyecto_infor_market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
