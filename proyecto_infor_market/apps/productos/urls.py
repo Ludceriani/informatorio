eec270c757a33f716c9ed83901601c105f605654
FrancoN.Durè <francodurenadir00@gmail.com>
2022-01-02 14:33:35 -0300

    forms errors

--- proyecto_infor_market/apps/productos/urls.py
+++ proyecto_infor_market/apps/productos/urls.py
@@ -6,5 +6,8 @@ app_name = "productos"
 
 urlpatterns = [
 	path("Detalle/", views.detalle, name="detalle"),
+
+	# Admin
 	path("Admin/Listar/", views.ListarAdmin.as_view(), name="admin_listar"),
+	path("Admin/Nuevo/", views.NuevoAdmin.as_view(), name= "admin_nuevo")
 ]
